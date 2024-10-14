404735e2884c1272ae3f20397d0679e9807324f1
czinx <cynthia.zinsou@epitech.eu>
2024-10-14 10:33:13 +0100

    Doc of rtype

---
+++ Doc/source/Guidelines/Main systems overviews.rst
@@ -0,0 +1,4 @@
+Main systems overviews
+======================
+
+This encompasses how the description of our system materializes the code
---
+++ Doc/source/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'The Developer Documentation'
+copyright = '2024, Cynthia Zinsou'
+author = 'Cynthia Zinsou'
+release = '2024'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+pygments_style = 'sphinx'
+highlight_language = 'cpp'
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
